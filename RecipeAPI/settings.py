f87afa5aa061f5494dcc756f817eb991bec9d073
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-10-24 00:14:48 +0500

    config db : added db config in django & added db deps in docker

--- RecipeAPI/settings.py
+++ RecipeAPI/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = "RecipeAPI.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": env("DB_HOST"),
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASS")
     }
 }
 
